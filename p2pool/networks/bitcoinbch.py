ecd2e0e716ac66f5058c6691110a878bb5ea89c8
Marian <amarian12@users.noreply.github.com>
2017-08-09 13:35:26 +0200

    BOOTSTRAP Update

--- p2pool/networks/bitcoinbch.py
+++ p2pool/networks/bitcoinbch.py
@@ -19,7 +19,7 @@ MIN_TARGET = 0
 MAX_TARGET = 2**256//2**32 - 1
 PERSIST = True
 WORKER_PORT = 5964
-BOOTSTRAP_ADDRS = 'p2pool.e-pool.net:5984 '.split(' ')
+BOOTSTRAP_ADDRS = 'p2pool.e-pool.net:5984 bch.coinminer.space:5984 31.214.242.111:5984'.split(' ')
 ANNOUNCE_CHANNEL = '#p2pool-alt'
 VERSION_CHECK = lambda v: None if 100000 <= v else 'Bitcoin version too old. Upgrade to 0.11.2 or newer!' # not a bug. BIP65 support is ensured by SOFTFORKS_REQUIRED
 VERSION_WARNING = lambda v: None
